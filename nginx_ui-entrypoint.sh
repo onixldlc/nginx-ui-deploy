bd3e36ebd2a24d9e25edf33680d708623097832a
onixldlc <81515667+onixldlc@users.noreply.github.com>
2025-04-07 01:55:03 +0700

    add: added default sites to avoid lockout and access via port 80

--- nginx_ui-entrypoint.sh
+++ nginx_ui-entrypoint.sh
@@ -6,6 +6,20 @@ if [ -z "$(ls -A /etc/nginx)" ]; then
     cp -r /etc/nginx-default/* /etc/nginx/
 fi
 
+# add all the config to the available sites
+cp /sites/default.conf /etc/nginx/sites-available/default
+cp /sites/nginx-ui.conf /etc/nginx/sites-available/nginx-ui
+
+# if env SERVER_NAME is set then replace the nginx-ui server_name and enable it
+if [ -n "$SERVER_NAME" ]; then
+    echo "Setting server_name to $SERVER_NAME"
+    sed -i "s/localhost/$SERVER_NAME/" /etc/nginx/sites-available/nginx-ui
+fi
+
+# enable the default sites
+ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/nginx-ui /etc/nginx/sites-enabled/nginx-ui
+
 # start nginx
 echo "Starting nginx..."
 nginx &
